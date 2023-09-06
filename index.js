43db49c0531c92db77ffd3e0ff9a6959fa9ab72a
himaloyash <himaloyashis98@gmail.com>
2023-09-07 00:47:55 +0600

    all ok

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ import { Server } from "socket.io";
 
 const port = process.env.PORT || 9000
 
-const io  = new Server(port, {
-    cors:{
-        origin:'http://localhost:5173',
-        methods:['GET', 'POST']
+const io = new Server(port, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
     }
 })
 
-io.on('connection', socket=>{
-    console.log('connected')
+io.on('connection', socket => {
+
+    socket.on('get-document', documentId => {
+
+        const data = ""
+        socket.join(documentId)
+        socket.emit('load-document', data)
+
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit('receive-changes', delta)
+
+        })
+
+    })
+
+
 })
