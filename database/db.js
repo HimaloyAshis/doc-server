91997a7abb5210c55f14c1758b5b65c3e1e3dff5
himaloyash <himaloyashis98@gmail.com>
2023-09-07 12:07:49 +0600

    env added

--- database/db.js
+++ database/db.js
@@ -1,2 +1,8 @@
+import mongoose from "mongoose";
+
 // pass = coK9IJ0RxlvXHII9
 // name = LikhoDoc
+
+const Connection = ()=>{
+    const url = `mongodb+srv://LikhoDoc:${process.env.DB_PASS}@cluster0.t0uxhcf.mongodb.net/?retryWrites=true&w=majority`
+}
